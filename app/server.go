1c744569268d8e04e5b4dbe4df06a0198543322a
Tanish chaurasia <tanishchaurasia56@gmail.com>
2024-07-06 01:29:48 +0530

    Making PING PONG Command

--- app/server.go
+++ app/server.go
@@ -13,14 +13,35 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	serve, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	defer serve.Close()
+
+	fmt.Println("Listening on port 6379")
+
+	conn, err := serve.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+
+	buf := make([]byte, 128)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
 }
