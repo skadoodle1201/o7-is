c613b5bccd5a55f422dac8a6e32fe0077b59e7d6
Tanish chaurasia <tanishchaurasia56@gmail.com>
2024-07-16 03:08:36 +0530

    Feat: added support for the empty RDB file for sync

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"net"
@@ -76,6 +77,11 @@ func handleConnection(conn net.Conn, role string) (err error) {
 
 		resMessage := commands.RedisCommands(operation.Value, args, role)
 		_, err = conn.Write([]byte(resMessage))
+		if operation.Value == "PSYNC" {
+			var emptyRDB, _ = hex.DecodeString("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
+			_, err = conn.Write(append([]byte(fmt.Sprintf("$%d\r\n", len(emptyRDB))), emptyRDB...))
+
+		}
 		if err != nil {
 			return fmt.Errorf("Error writing: %v", err.Error())
 		}
